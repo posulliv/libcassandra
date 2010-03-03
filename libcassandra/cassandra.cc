53fecea4835737b7277b780878cb62608fbbb7f2
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2010-03-03 16:50:52 -0500

    Added an emtpy C++ source file.

---
+++ libcassandra/cassandra.cc
@@ -0,0 +1,8 @@
+/*
+ * LibCassandra
+ * Copyright (C) 2010 Padraig O'Sullivan
+ * All rights reserved.
+ *
+ * Use and distribution licensed under the BSD license. See
+ * the COPYING file in the parent directory for full text.
+ */
