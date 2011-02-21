04172766393e28f77fdab73f9a9a567413987188
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2011-02-21 14:34:01 -0500

    Updated examples based on new api.

--- examples/simple.cc
+++ examples/simple.cc
@@ -8,7 +8,9 @@
 
 #include <libcassandra/cassandra_factory.h>
 #include <libcassandra/cassandra.h>
+#include <libcassandra/column_family_definition.h>
 #include <libcassandra/keyspace.h>
+#include <libcassandra/keyspace_definition.h>
 
 using namespace std;
 using namespace libcassandra;
@@ -24,27 +26,30 @@ int main()
   string clus_name= client->getClusterName();
   cout << "cluster name: " << clus_name << endl;
 
-  set<string> key_out= client->getKeyspaces();
-  for (set<string>::iterator it = key_out.begin(); it != key_out.end(); ++it)
+  vector<KeyspaceDefinition> key_out= client->getKeyspaces();
+  for (vector<KeyspaceDefinition>::iterator it = key_out.begin(); it != key_out.end(); ++it)
   {
-    cout << "keyspace: " << *it << endl;
+    cout << "keyspace: " << (*it).getName() << endl;
   }
 
-  map<string, string> tokens= client->getTokenMap(false);
-  for (map<string, string>::iterator it= tokens.begin();
-       it != tokens.end();
-       ++it)
-  {
-    cout << it->first << " : " << it->second << endl;
-  }
-
-  tr1::shared_ptr<Keyspace> key_space= client->getKeyspace("drizzle");
-  /* insert data */
   try
   {
-    key_space->insertColumn("sarah", "Data", "third", "this is data being inserted!");
+    /* create keyspace */
+    KeyspaceDefinition ks_def;
+    ks_def.setName("drizzle");
+    client->createKeyspace(ks_def);
+    client->setKeyspace(ks_def.getName());
+
+    /* create standard column family */
+    ColumnFamilyDefinition cf_def;
+    cf_def.setName("Data");
+    cf_def.setKeyspaceName(ks_def.getName());
+    client->createColumnFamily(cf_def);
+
+    /* insert data */
+    client->insertColumn("sarah", "Data", "third", "this is data being inserted!");
     /* retrieve that data */
-    string res= key_space->getColumnValue("sarah", "Data", "third");
+    string res= client->getColumnValue("sarah", "Data", "third");
     cout << "Value in column retrieved is: " << res << endl;
   }
   catch (org::apache::cassandra::InvalidRequestException &ire)
